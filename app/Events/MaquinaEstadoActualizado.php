90e9df2543bd76cd668074fda80201ece5a4af9a
DouglasDFH <douglasdfh88@gmail.com>
2025-11-20 19:58:30 -0400

    Actualizar la clase MaquinaEstadoActualizado para implementar ShouldBroadcastNow en lugar de ShouldBroadcast

--- app/Events/MaquinaEstadoActualizado.php
+++ app/Events/MaquinaEstadoActualizado.php
@@ -4,14 +4,12 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\MaquinaEstadoVivo;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class MaquinaEstadoActualizado implements ShouldBroadcast
+class MaquinaEstadoActualizado implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
